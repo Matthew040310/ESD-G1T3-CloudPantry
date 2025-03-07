f1203d1237c6b9af40a0c98aa35ad4b9a0d38dca
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-03-07 16:31:17 +0800

    Added test cases for delete function
    testcases for single and multiple deletes successful
    
    Of note, my update function uses the upsert command by supabase. This will update existing entry, or create new one if it doesnt exist. The reason why i have two different functions for create and update is because upsert does not have auto generation of id.
    
    Because of the nature of upsert, it runs asych with the delete function, and the item entries may remain even after the delete test cases are successful. Hence, the teardownclass function is there to run after all testcases have been completed, to clean up the database

--- InventoryDatabase/addUpdateDeleteTest.py
+++ InventoryDatabase/addUpdateDeleteTest.py
@@ -106,27 +106,30 @@ class addUpdateDeleteTest(unittest.TestCase):
     # Test updating the details of more than one item
     def test_updateInventory2(self):
         data = [{
-                        "Expiry Date": "03-07-2025",
-                        "Fill Factor": 2,
                         "Name": "Modified Item B1",
+                        "Fill Factor": 2,
                         "Quantity": 0,
+                        "CharityID": 1,
                         "Type": "Fats",
+                        "Expiry Date": "03-07-2025",
                         "ID":addUpdateDeleteTest.test_itemB1_id
                     },
                     {
-                        "Expiry Date": "03-07-2025",
-                        "Fill Factor": 2,
                         "Name": "Modified Item B2",
+                        "Fill Factor": 2,
                         "Quantity": 0,
+                        "CharityID": 1,
                         "Type": "Fats",
+                        "Expiry Date": "03-07-2025",
                         "ID":addUpdateDeleteTest.test_itemB2_id
                     },
                     {
-                        "Expiry Date": "03-07-2025",
-                        "Fill Factor": 2,
                         "Name": "Modified Date Item",
+                        "Fill Factor": 2,
                         "Quantity": 0,
+                        "CharityID": 1,
                         "Type": "Fats",
+                        "Expiry Date": "03-07-2025",
                         "ID":addUpdateDeleteTest.test_date_id
                     }]
         response = self.app.put('/inventory/0',json=data)
@@ -136,6 +139,69 @@ class addUpdateDeleteTest(unittest.TestCase):
         json_data = response.get_json()
         self.assertEqual(json_data['code'], 200)
         self.assertEqual(json_data['data']['total_count'], 3)
+
+    # Test deleting the details of one item
+    def test_deleteInventoryItem1(self):
+        data = [{
+                        "Name": "Modified Item A",
+                        "ID":addUpdateDeleteTest.test_itemA_id
+                    }]
+        response = self.app.delete('/inventory',json=data)
+            
+        # Assert the response status code and data
+        self.assertEqual(response.status_code, 200)
+        json_data = response.get_json()
+        self.assertEqual(json_data['code'], 200)
+        self.assertEqual(json_data['data']['total_count'], 1)
+
+    # Test deleting more than one item. Clears remaining test items used in this test file
+    def test_deleteTestItems(self):
+        data = [{
+                        "Name": "Modified Item B1",
+                        "ID":addUpdateDeleteTest.test_itemB1_id
+                    },
+                    {
+                        "Name": "Modified Item B2",
+                        "ID":addUpdateDeleteTest.test_itemB2_id
+                    },
+                    {
+                        "Name": "Modified Date Item",
+                        "ID":addUpdateDeleteTest.test_date_id
+                    }]
+        response = self.app.delete('/inventory',json=data)
+            
+        # Assert the response status code and data
+        self.assertEqual(response.status_code, 200)
+        json_data = response.get_json()
+        self.assertEqual(json_data['code'], 200)
+        self.assertEqual(json_data['data']['total_count'], 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        testids = [
+                    {
+                        "Name": "Modified Item A",
+                        "ID":addUpdateDeleteTest.test_itemA_id
+                    },
+                    {
+                        "Name": "Modified Item B1",
+                        "ID":addUpdateDeleteTest.test_itemB1_id
+                    },
+                    {
+                        "Name": "Modified Item B2",
+                        "ID":addUpdateDeleteTest.test_itemB2_id
+                    },
+                    {
+                        "Name": "Modified Date Item",
+                        "ID":addUpdateDeleteTest.test_date_id
+                    }]
+        response = cls.app.delete('/inventory',json=testids)
+        # Assert response status code
+        assert response.status_code == 200
+        json_data = response.get_json()
+        # Assert all 4 test cases were removed
+        assert json_data['data']['total_count'] == 4
+        
 
 # Running the unittests (run code within python environment)
 unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromTestCase(addUpdateDeleteTest))
