e64bf93813ffb25341f1b0b118a30b2a63b820b2
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-03-07 13:09:07 +0800

    Created getCharityInventory Function
    added getCharityInventory() to inventory
    made successful_result function to jsonify all successful responses (avoid repeated code)
    
    added testcase for new function
    amended testcases, removing mockup (not required for our use case)

--- InventoryDatabase/inventory.py
+++ InventoryDatabase/inventory.py
@@ -12,15 +12,25 @@ SUPABASE_URL: str = os.getenv('SUPABASE_URL')
 
 # To change code later for auto retrieval from docker yaml, so that code can be reused for excess_inventory table
 # TARGET_TABLE: str = os.getenv('TABLE_NAME')
-# CHARITY_ID: int = os.getenv("CHARITY_ID")
 target_table = "Inventory"
-charity_id = ""
 
 # Connect to database
 supabase : Client = create_client(SUPABASE_URL, SUPABASE_API_KEY)
 
 # Initialise Flask Application
 app = Flask(__name__)
+
+# To jsonify successful results
+def successful_result(response):
+    return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "response": response.data,
+                    "total_count": len(response.data)
+                    },
+            }
+        ), 200
 
 @app.route("/inventory")
 def getAllInventory():
@@ -30,15 +40,7 @@ def getAllInventory():
             .select("*")
             .execute()
         )
-        return jsonify(
-            {
-                "code": 200,
-                "data": {
-                    "response": response.data,
-                    "total_count": len(response.data)
-                    },
-            }
-        ), 200
+        return successful_result(response)
     except Exception as e:
         return jsonify(
             {
@@ -47,7 +49,23 @@ def getAllInventory():
                 }
         ), 400
 
-
+@app.route("/inventory/<int:charityID>")
+def getCharityInventory(charityID):
+    try:
+        response = (
+            supabase.table(target_table)
+            .select("*")
+            .eq("charityID",charityID)
+            .execute()
+        )
+        return successful_result(response)
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 400,
+                "message": f"Error Occurred.\n{e.message}"
+                }
+        ), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- InventoryDatabase/inventoryTest.py
+++ InventoryDatabase/inventoryTest.py
@@ -1,5 +1,4 @@
 import unittest
-from unittest.mock import patch
 import inventory
 
 class TestInventory(unittest.TestCase):
@@ -8,12 +7,7 @@ class TestInventory(unittest.TestCase):
         self.app.testing = True
 
     def test_getAllInventory(self):
-        with patch('inventory.getAllInventory') as mock_getAllInventory:
-            mock_getAllInventory.return_value = (
-                {
-                    "code": 200,
-                    "data": {
-                        "response": [{
+        expected_data_response = [{
                                     "charityID": 0,
                                     "expiry_date": "2025-03-07",
                                     "fill_factor": 2,
@@ -30,21 +24,37 @@ class TestInventory(unittest.TestCase):
                                     "name": "Chicken",
                                     "quantity": 1,
                                     "type": "Protein"
-                                }],
-                        "total_count": 2
-                    },
-                },
-                200
-            )
-            # Make a GET request to the `/inventory` route
-            response = self.app.get('/inventory')
+                                }]
+        # Make a GET request to the `/inventory` route
+        response = self.app.get('/inventory')
             
-            # Assert the response status code and data
-            self.assertEqual(response.status_code, 200)
-            json_data = response.get_json()
-            self.assertEqual(json_data['code'], 200)
-            self.assertEqual(len(json_data['data']['response']), 2)
-            self.assertEqual(json_data['data']['total_count'], 2)
+        # Assert the response status code and data
+        self.assertEqual(response.status_code, 200)
+        json_data = response.get_json()
+        self.assertEqual(json_data['code'], 200)
+        self.assertEqual(json_data['data']['response'], expected_data_response)
+        self.assertEqual(json_data['data']['total_count'], 2)
+
+    def test_getCharityInventory(self):
+        expected_data_response = [{
+                        "charityID": 0,
+                        "expiry_date": "2025-03-07",
+                        "fill_factor": 2,
+                        "id": "ba3f8d20-de29-425d-aab7-7a42d5f10909",
+                        "name": "Bread",
+                        "quantity": 1,
+                        "type": "Carbs"
+                    }]
+
+        # Make a GET request to the `/inventory/<CharityID>` route
+        response = self.app.get('/inventory/0')
+            
+        # Assert the response status code and data
+        self.assertEqual(response.status_code, 200)
+        json_data = response.get_json()
+        self.assertEqual(json_data['code'], 200)
+        self.assertEqual(json_data['data']['response'], expected_data_response)
+        self.assertEqual(json_data['data']['total_count'], 1)
 
 # Running the unittests (run code within python environment)
 unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromTestCase(TestInventory))
