9bf9ba9a3d96c9ee104fff79afc3c04d5efb0b64
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-03-24 21:02:23 +0800

    (info) : Updated test cases for new data in database

--- backend/Inventory/inventoryTest.py
+++ backend/Inventory/inventoryTest.py
@@ -7,6 +7,15 @@ class TestInventory(unittest.TestCase):
         self.app.testing = True
 
     def test_getAllInventory(self):
+        # Make a GET request to the `/inventory` route
+        response = self.app.get('/inventory')
+            
+        # Assert the response status code and data
+        self.assertEqual(response.status_code, 200)
+        json_data = response.get_json()
+        self.assertEqual(json_data['data']['total_count'], 4)
+
+    def test_getCharityInventory(self):
         expected_data_response = [
                                 {
                                     "charityID": 1,
@@ -14,52 +23,18 @@ class TestInventory(unittest.TestCase):
                                     "fill_factor": 2,
                                     "id": "a600288e-27ea-4d73-a4a5-af106b567504",
                                     "name": "Test Olive Oil",
-                                    "quantity": 300,
+                                    "quantity": 150,
                                     "restrictions": None,
                                     "category": "Cooking Essentials",
                                     "type": "Fats"
-                                },
-                                {
-                                    "charityID": 0,
-                                    "expiry_date": "2025-03-07",
-                                    "fill_factor": 2,
-                                    "id": "ba3f8d20-de29-425d-aab7-7a42d5f10909",
-                                    "name": "Test Pasta",
-                                    "quantity": 200,
-                                    "restrictions": ["Halal","Vegetarian"],
-                                    "category": "Pasta & Grains",
-                                    "type": "Carbs"
                                 }]
-        # Make a GET request to the `/inventory` route
-        response = self.app.get('/inventory')
-            
-        # Assert the response status code and data
-        self.assertEqual(response.status_code, 200)
-        json_data = response.get_json()
-        self.assertEqual(json_data['code'], 200)
-        self.assertCountEqual(json_data['data']['response'], expected_data_response)
-        self.assertEqual(json_data['data']['total_count'], 2)
-
-    def test_getCharityInventory(self):
-        expected_data_response = [{
-                        "charityID": 0,
-                        "expiry_date": "2025-03-07",
-                        "fill_factor": 2,
-                        "id": "ba3f8d20-de29-425d-aab7-7a42d5f10909",
-                        "name": "Test Pasta",
-                        "quantity": 200,
-                        "restrictions": ["Halal","Vegetarian"],
-                        "category": "Pasta & Grains",
-                        "type": "Carbs"
-                    }]
 
         # Make a GET request to the `/inventory/<CharityID>` route
-        response = self.app.get('/inventory/0')
+        response = self.app.get('/inventory/1')
             
         # Assert the response status code and data
         self.assertEqual(response.status_code, 200)
         json_data = response.get_json()
-        self.assertEqual(json_data['code'], 200)
         self.assertEqual(json_data['data']['response'], expected_data_response)
         self.assertEqual(json_data['data']['total_count'], 1)
 
@@ -70,7 +45,6 @@ class TestInventory(unittest.TestCase):
         response = self.app.get('/restrictions')
             
         # Assert the response status code and data
-        print(response)
         self.assertEqual(response.status_code, 200)
         json_data = response.get_json()
         self.assertCountEqual(json_data, expected_data_response)
