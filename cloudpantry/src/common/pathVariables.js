87bc199de479046d5a0e26600e1f69d04a987b5a
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-03-27 13:26:42 +0800

    (feat) : Create pathVariables.js
    Centralised location to edit port numbers across frontend

---
+++ cloudpantry/src/common/pathVariables.js
@@ -0,0 +1,8 @@
+export const INVENTORY_PORT = 5000;
+export const EXCESS_INVENTORY_PORT = 5001;
+export const NOTIFICATION_PORT = 5002;
+
+
+export const INVENTORY_URL = `http://localhost:${INVENTORY_PORT}/inventory`;
+export const EXCESS_INVENTORY_URL = `http://localhost:${EXCESS_INVENTORY_PORT}/inventory`;
+export const NOTIFICATION_URL = `http://localhost:${NOTIFICATION_PORT}/notification`;
