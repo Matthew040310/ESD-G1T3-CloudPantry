206b93f8e9acd816e501d2909a1d76e11c545bde
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-04-03 11:03:35 +0800

    refactor : Merge route microservice variable
    pathVariables serves as the centralised source of variables across .js files

--- cloudpantry/src/common/pathVariables.js
+++ cloudpantry/src/common/pathVariables.js
@@ -1,8 +1,10 @@
 export const INVENTORY_PORT = 5006;
 export const EXCESS_INVENTORY_PORT = 5001;
 export const NOTIFICATION_PORT = 5002;
+export const ONEMAP_PORT = 5003;
 
 
 export const INVENTORY_URL = `http://localhost:${INVENTORY_PORT}/inventory`;
 export const EXCESS_INVENTORY_URL = `http://localhost:${EXCESS_INVENTORY_PORT}/inventory`;
 export const NOTIFICATION_URL = `http://localhost:${NOTIFICATION_PORT}/notification`;
+export const ONEMAP_URL = `http://localhost:${ONEMAP_PORT}/notification`;
